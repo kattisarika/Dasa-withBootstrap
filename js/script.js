11c7a563d045958a115ac8be486b12d7951f4116
kattisarika <sarika.katti@gmail.com>
2018-04-07 13:28:35 -0700

    Enhancements to javascript , index and style, css

--- js/script.js
+++ js/script.js
@@ -1,10 +1,55 @@
 jQuery(document).ready(function($) {
 		$('.showsong1').hide()
+		$('.contactus').hide()
+		$('.yoursong').hide()
+		$('.login').hide()
 		$('a[href^="#"]').on('click', function(event) {
 			$('.showsong').hide();
 			$('.showsong1') .hide();
    			var target = $(this).attr('href');
     		$('.showsong1'+target).toggle();
 		});
-
+		
 	});	
+
+$(function() {
+   
+    $("#home").on("click",function(e){
+    	$(".yoursong").hide()
+    	$(".login").hide()
+    	$(".contactus").hide()
+    	$('.showsong1') .hide();
+    	$('.showsong').show();
+
+
+    });
+
+ 	$("#yoursong").on("click",function(e) {
+  		$('.showsong').hide();
+		$('.showsong1') .hide();
+		$(".contactus").hide()
+		$(".login").hide()
+		$(".yoursong").show();
+  	});
+
+
+
+  	$("#login").on("click",function(e) {
+  		$('.showsong').hide();
+		$('.showsong1') .hide();
+		$(".yoursong").hide()
+		$(".contactus").hide()
+		$(".login").show()
+  	});
+
+   
+   $('#contactus').on("click",function(e){
+   		$('.showsong').hide();
+		$('.showsong1') .hide();
+		$(".yoursong").hide()
+		$(".login").hide()
+		$(".contactus").show()
+
+   });
+
+});
