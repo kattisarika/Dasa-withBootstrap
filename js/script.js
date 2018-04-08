859dd318872fc53dca843a833a33721f84471683
kattisarika <sarika.katti@gmail.com>
2018-04-07 17:20:56 -0700

    made changes to index, style.css, script.js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,59 @@
-jQuery(document).ready(function($) {
-		$('.showsong1').hide()
-		$('.contactus').hide()
-		$('.yoursong').hide()
-		$('.login').hide()
-		$('a[href^="#"]').on('click', function(event) {
-			$('.showsong').hide();
-			$('.showsong1') .hide();
-   			var target = $(this).attr('href');
-    		$('.showsong1'+target).toggle();
-		});
-		
-	});	
+jQuery(document).ready(function ($) {
+    $('.showsong1').hide()
+    $('.contactus').hide()
+    $('.yoursong').hide()
+    $('.login').hide()
+    $('a[href^="#"]').on('click', function (event) {
+        $(".showsong").hide();
+        $('.contactus').hide()
+        $('.yoursong').hide()
+        $('.login').hide()
+        var target = $(this).attr('href');
+        $('.showsong1' + target).toggle();
+    });
 
-$(function() {
-   
-    $("#home").on("click",function(e){
-    	$(".yoursong").hide()
-    	$(".login").hide()
-    	$(".contactus").hide()
-    	$('.showsong1') .hide();
-    	$('.showsong').show();
+});
+
+$(function () {
+
+    $("#home").on("click", function (e) {
+        $(".yoursong").hide()
+        $(".login").hide()
+        $(".contactus").hide()
+        $(".showsong1").hide();
+        $(".showsong").show();
 
 
     });
 
- 	$("#yoursong").on("click",function(e) {
-  		$('.showsong').hide();
-		$('.showsong1') .hide();
-		$(".contactus").hide()
-		$(".login").hide()
-		$(".yoursong").show();
-  	});
+    $("#yoursong").on("click", function (e) {
+        $('.showsong').hide()
+        $('.showsong1').hide()
+        $(".contactus").hide()
+        $(".login").hide()
+        $(".yoursong").show();
+    });
 
 
 
-  	$("#login").on("click",function(e) {
-  		$('.showsong').hide();
-		$('.showsong1') .hide();
-		$(".yoursong").hide()
-		$(".contactus").hide()
-		$(".login").show()
-  	});
+    $("#login").on("click", function (e) {
+        $(".showsong").hide()
+        $(".showsong1").hide()
+        $(".yoursong").hide()
+        $(".contactus").hide()
+        $(".login").show()
+    });
 
-   
-   $('#contactus').on("click",function(e){
-   		$('.showsong').hide();
-		$('.showsong1') .hide();
-		$(".yoursong").hide()
-		$(".login").hide()
-		$(".contactus").show()
 
-   });
+    $('#contactus').on("click", function (e) {
+        $(".showsong").hide()
+        $(".showsong1").hide()
+        $(".yoursong").hide()
+        $(".login").hide()
+        $(".contactus").show()
+
+    });
+
+
 
 });
